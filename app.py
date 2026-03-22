9792b7a1ff0baee1e76e7773b3e90919c368d2de
Inês Martinelli <268224780+inamartinelli-bit@users.noreply.github.com>
2026-03-22 10:29:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@
 aparelho = input("Insira o nome do aparelho elétrico utilizado: ")
 potencia = int(input("Insira a potência do aparelho em watts (W): "))
 horasDia = float(input("Insira o tempo médio de uso diário em horas (h): "))
-"""Processamento: 
-1) Cálculo do consumo mensal (estimado) em kWh/mês;
-2) Cálculo do custo mensal estimado em kWh, dado pelo consumo mensal multiplicado pelo valor fixo de 0.65 centavos de Real (R$)."""
+# Processamento: 
+# 1) Cálculo do consumo mensal (estimado) em kWh/mês;
+# 2) Cálculo do custo mensal estimado em kWh, dado pelo consumo mensal multiplicado pelo valor fixo de 0.65 centavos de Real (R$).
 consumoMensal = (potencia * horasDia * 30) / 1000
 gastomensal = (consumoMensal * 0.35)
 # Saída
